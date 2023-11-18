5c24b2a5e35db6f43b9c756b19672acafd428f3c
siweixie <146999962+siweixie@users.noreply.github.com>
2023-11-18 12:46:43 -0500

    Update One_box_3.py

--- One_box_3.py
+++ One_box_3.py
@@ -16,7 +16,7 @@ def model_109(G, Q, Q_L, epsilon_L, epsilon_L_F, V, t_g, T):
     C_rise = [(G * (1 - epsilon_L * epsilon_L_F) / (Q + epsilon_L_F * Q_L)) * (1 - np.exp(-(Q + epsilon_L_F * Q_L) * t / V)) if t <= t_g else np.nan for t in time_vector]
     C0 = (G * (1 - epsilon_L * epsilon_L_F)) / (Q + epsilon_L_F * Q_L) * (1 - np.exp(-(Q + epsilon_L_F * Q_L) * t_g / V))
     C_decay = [C0 * np.exp(-(Q + epsilon_L_F * Q_L) * (t - t_g) / V) if t > t_g else np.nan for t in time_vector]
-    return time_vector, np.array([x if not np.isnan(x) else y for x, y in zip(C_rise, C_decay)])
+    return time_vector, C_rise, C_decay
 
 def model_110(G, Q, Q_L, epsilon_L, epsilon_L_F, Q_R, epsilon_RF, gamma):
     C_avg = (gamma * G * (1 - epsilon_L * epsilon_L_F)) / ((Q + epsilon_RF * Q_R) + epsilon_L_F * Q_L)
@@ -27,8 +27,8 @@ def model_110(G, Q, Q_L, epsilon_L, epsilon_L_F, Q_R, epsilon_RF, gamma):
 
 def model_111(G, Q, Q_L, epsilon_L, epsilon_L_F, Q_R, epsilon_RF, V, t_g, T):
     Q_instead = Q + epsilon_RF * Q_R
-    time_vector, concentration = model_109(G, Q_instead, Q_L, epsilon_L, epsilon_L_F, V, t_g, T)
-    return time_vector, concentration
+    return model_109(G, Q_instead, Q_L, epsilon_L, epsilon_L_F, V, t_g, T)
+
 
 # Set the parameters
 T = 60
@@ -43,60 +43,58 @@ epsilon_RF = 0.9
 V = 100
 gamma = 0.25  
 
-# Perform calculations
-time_109, concentration_109 = model_109(G, Q, Q_L, epsilon_L, epsilon_L_F, V, t_g, T)
-time_111, concentration_111 = model_111(G, Q, Q_L, epsilon_L, epsilon_L_F, Q_R, epsilon_RF, V, t_g, T)
 
-# Visualization: Model 109 vs Model 111
-plt.figure(figsize=(10, 5))
-plt.plot(time_109, concentration_109, label='Model 109')
-plt.plot(time_111, concentration_111, label='Model 111', linestyle='--')
-plt.title('Model 109 vs Model 111')
-plt.xlabel('Time (minutes)')
-plt.ylabel('Concentration (mg/m^3)')
-plt.legend()
-plt.show()
-
-# Calculate Mean Squared Error and R^2
-mse = np.mean((concentration_109 - concentration_111)**2)
-r2 = np.corrcoef(concentration_109, concentration_111)[0, 1]**2
-
-# Statistical Analysis
-normality_test_109 = shapiro(concentration_109[~np.isnan(concentration_109)])
-normality_test_111 = shapiro(concentration_111[~np.isnan(concentration_111)])
-
-wilcoxon_test = None
-if normality_test_109.pvalue < 0.05 and normality_test_111.pvalue < 0.05:
-    wilcoxon_test = wilcoxon(concentration_109[~np.isnan(concentration_109)], concentration_111[~np.isnan(concentration_111)])
-    
-mse, r2, normality_test_109, normality_test_111, wilcoxon_test
+# Calculate Results
+results_108 = model_108(G, Q, Q_L, epsilon_L, epsilon_L_F, gamma)
+results_109 = model_109(G, Q, Q_L, epsilon_L, epsilon_L_F, V, t_g, T)
+results_110 = model_110(G, Q, Q_L, epsilon_L, epsilon_L_F, Q_R, epsilon_RF, gamma)
+results_111 = model_111(G, Q, Q_L, epsilon_L, epsilon_L_F, Q_R, epsilon_RF, V, t_g, T)
 
 
 # Prepare Data for Plotting and Comparison
-# Convert all model results to DataFrame in a consistent format
+time_109, C_rise_109, C_decay_109 = results_109
+time_111, C_rise_111, C_decay_111 = results_111
+
 results_109_df = pd.DataFrame({
     'Time': np.concatenate([time_109, time_109]),
-    'Concentration': np.concatenate([concentration_109, concentration_109]),
+    'Concentration': np.concatenate([C_rise_109, C_decay_109]),
     'Model': 'Model 109'
 }).dropna()
 
 results_111_df = pd.DataFrame({
     'Time': np.concatenate([time_111, time_111]),
-    'Concentration': np.concatenate([concentration_111, concentration_111]),
+    'Concentration': np.concatenate([C_rise_111, C_decay_111]),
     'Model': 'Model 111'
 }).dropna()
 
-results_101_df = pd.DataFrame({
-    'Time': np.concatenate([time_101, time_101]),
-    'Concentration': np.concatenate([concentration_101, concentration_101]),
-    'Model': 'Model 101'
-}).dropna()
 
-results_103_df = pd.DataFrame({
-    'Time': np.concatenate([time_103, time_103]),
-    'Concentration': np.concatenate([concentration_103, concentration_103]),
-    'Model': 'Model 103'
-}).dropna()
+# Plotting
+plt.figure(figsize=(10, 6))
+plt.plot(results_109_df['Time'], results_109_df['Concentration'], label='Model 109')
+plt.plot(results_111_df['Time'], results_111_df['Concentration'], label='Model 111', linestyle='dashed')
+plt.title('Comparison of Model 109 and Model 111')
+plt.xlabel('Time (minutes)')
+plt.ylabel('Concentration (mg/m^3)')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# Calculate Mean Squared Error and R^2
+mse = np.mean((results_109_df['Concentration'] - results_111_df['Concentration'])**2)
+r2 = np.corrcoef(results_109_df['Concentration'], results_111_df['Concentration'])[0, 1]**2
+
+# Normality Test
+normality_test_109 = stats.shapiro(results_109_df['Concentration'])
+normality_test_111 = stats.shapiro(results_111_df['Concentration'])
+
+# Wilcoxon Signed-Rank Test
+wilcoxon_test = stats.wilcoxon(results_109_df['Concentration'], results_111_df['Concentration'])
+
+mse, r2, normality_test_109, normality_test_111, wilcoxon_test
+
+
+# Prepare Data for Plotting and Comparison
+# Convert all model results to DataFrame in a consistent format
 
 # Combine all data for visualization
 all_data_df = pd.concat([
